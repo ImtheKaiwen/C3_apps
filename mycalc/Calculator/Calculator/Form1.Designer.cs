84e985bbe51e972f798291dd53dcf94907e4893f
Kerem <bucocuknereli@gmail.com>
2023-12-28 11:54:11 +0300

    Add files via upload

---
+++ mycalc/Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.but7 = new System.Windows.Forms.Button();
+            this.but8 = new System.Windows.Forms.Button();
+            this.but9 = new System.Windows.Forms.Button();
+            this.but6 = new System.Windows.Forms.Button();
+            this.but5 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.but1 = new System.Windows.Forms.Button();
+            this.but2 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.butx = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.esittir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // but7
+            // 
+            this.but7.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but7.Location = new System.Drawing.Point(50, 151);
+            this.but7.Name = "but7";
+            this.but7.Size = new System.Drawing.Size(78, 81);
+            this.but7.TabIndex = 0;
+            this.but7.Text = "7";
+            this.but7.UseVisualStyleBackColor = true;
+            this.but7.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // but8
+            // 
+            this.but8.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but8.Location = new System.Drawing.Point(134, 151);
+            this.but8.Name = "but8";
+            this.but8.Size = new System.Drawing.Size(78, 81);
+            this.but8.TabIndex = 0;
+            this.but8.Text = "8";
+            this.but8.UseVisualStyleBackColor = true;
+            this.but8.Click += new System.EventHandler(this.but8_Click);
+            // 
+            // but9
+            // 
+            this.but9.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but9.Location = new System.Drawing.Point(219, 151);
+            this.but9.Name = "but9";
+            this.but9.Size = new System.Drawing.Size(78, 81);
+            this.but9.TabIndex = 0;
+            this.but9.Text = "9";
+            this.but9.UseVisualStyleBackColor = true;
+            this.but9.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // but6
+            // 
+            this.but6.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but6.Location = new System.Drawing.Point(219, 238);
+            this.but6.Name = "but6";
+            this.but6.Size = new System.Drawing.Size(78, 81);
+            this.but6.TabIndex = 0;
+            this.but6.Text = "6";
+            this.but6.UseVisualStyleBackColor = true;
+            this.but6.Click += new System.EventHandler(this.but6_Click);
+            // 
+            // but5
+            // 
+            this.but5.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but5.Location = new System.Drawing.Point(134, 238);
+            this.but5.Name = "but5";
+            this.but5.Size = new System.Drawing.Size(78, 81);
+            this.but5.TabIndex = 0;
+            this.but5.Text = "5";
+            this.but5.UseVisualStyleBackColor = true;
+            this.but5.Click += new System.EventHandler(this.but5_Click);
+            // 
+            // but4
+            // 
+            this.but4.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but4.Location = new System.Drawing.Point(50, 238);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(78, 81);
+            this.but4.TabIndex = 0;
+            this.but4.Text = "4";
+            this.but4.UseVisualStyleBackColor = true;
+            this.but4.Click += new System.EventHandler(this.but4_Click);
+            // 
+            // but1
+            // 
+            this.but1.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but1.Location = new System.Drawing.Point(50, 326);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(78, 81);
+            this.but1.TabIndex = 0;
+            this.but1.Text = "1";
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // but2
+            // 
+            this.but2.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but2.Location = new System.Drawing.Point(134, 326);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(78, 81);
+            this.but2.TabIndex = 0;
+            this.but2.Text = "2";
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // but3
+            // 
+            this.but3.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but3.Location = new System.Drawing.Point(219, 326);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(78, 81);
+            this.but3.TabIndex = 0;
+            this.but3.Text = "3";
+            this.but3.UseVisualStyleBackColor = true;
+            this.but3.Click += new System.EventHandler(this.but3_Click);
+            // 
+            // butx
+            // 
+            this.butx.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butx.Location = new System.Drawing.Point(304, 151);
+            this.butx.Name = "butx";
+            this.butx.Size = new System.Drawing.Size(78, 81);
+            this.butx.TabIndex = 0;
+            this.butx.Text = "x";
+            this.butx.UseVisualStyleBackColor = true;
+            this.butx.Click += new System.EventHandler(this.butx_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(304, 238);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(78, 81);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "+";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Trebuchet MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(304, 326);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(78, 81);
+            this.button12.TabIndex = 0;
+            this.button12.Text = "-";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(304, 414);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(78, 81);
+            this.button13.TabIndex = 0;
+            this.button13.Text = "/";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("Trebuchet MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.Location = new System.Drawing.Point(219, 414);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(78, 81);
+            this.button14.TabIndex = 0;
+            this.button14.Text = ".";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button15.Location = new System.Drawing.Point(134, 414);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(78, 81);
+            this.button15.TabIndex = 0;
+            this.button15.Text = "0";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // esittir
+            // 
+            this.esittir.Font = new System.Drawing.Font("Trebuchet MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.esittir.Location = new System.Drawing.Point(50, 414);
+            this.esittir.Name = "esittir";
+            this.esittir.Size = new System.Drawing.Size(78, 81);
+            this.esittir.TabIndex = 0;
+            this.esittir.Text = "=";
+            this.esittir.UseVisualStyleBackColor = true;
+            this.esittir.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Trebuchet MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(50, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(418, 72);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(389, 151);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(78, 344);
+            this.clear.TabIndex = 0;
+            this.clear.Text = "clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 540);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.esittir);
+            this.Controls.Add(this.but1);
+            this.Controls.Add(this.but4);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.but2);
+            this.Controls.Add(this.but5);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.but3);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.but6);
+            this.Controls.Add(this.butx);
+            this.Controls.Add(this.but9);
+            this.Controls.Add(this.but8);
+            this.Controls.Add(this.but7);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button but7;
+        private System.Windows.Forms.Button but8;
+        private System.Windows.Forms.Button but9;
+        private System.Windows.Forms.Button but6;
+        private System.Windows.Forms.Button but5;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button butx;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button esittir;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ mycalc/Calculator/Calculator/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "-";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ",";
+        }
+
+        private void but3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void but4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void but5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void but6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void but8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            string number1="", number2="",islem="";
+            string text = textBox1.Text;
+            for (int i=0; i<=text.Length-1;i++)
+            {
+                if (i==0) // x / - + may only - 
+                {
+                    number1 += text[i];
+                }
+                else
+                {
+                    if (text[i] != 'x' && text[i] != '+' && text[i] != '-' && text[i] != '/')
+                    {
+                        number1 += text[i];
+
+                    }
+                    if (text[i] == 'x') { islem = "carpma"; break; }
+                    if (text[i] == '/') { islem = "bolme"; break; }
+                    if (text[i] == '-') { islem = "cıkarma"; break; }
+                    if (text[i] == '+') { islem = "toplama"; break; }
+                }
+            }
+            for (int i=text.Length-1; i>=0; i--)
+            {
+                if (text[i] != 'x' && text[i] != '+' && text[i] != '-' && text[i] != '/')
+                {
+                    number2 += text[i];
+                }
+                else
+                {
+                    string number3="";
+                    for (i = number2.Length-1 ; i >=0; i--)
+                    {
+                        number3 += number2[i];
+                    }
+                    number2= number3;
+                    break;
+                }
+            }
+            switch (islem)
+            {
+                case "carpma":
+                    float number11 = float.Parse(number1);
+                    float number12 = float.Parse(number2);
+                    float multiplication = number11 * number12;
+                    textBox1.Text = multiplication.ToString();
+                    break;
+                case "bolme":
+                    float number21 = float.Parse(number1);
+                    float number22 = float.Parse(number2);
+                    float division = number21 / number22;
+                    textBox1.Text = division.ToString();
+                    break;
+                case "cıkarma":
+                    //codes
+                    float number31 = float.Parse(number1);
+                    float number32 = float.Parse(number2);
+                    float extraction = number31 - number32;
+                    textBox1.Text = extraction.ToString();
+                    break;
+                case "toplama":
+                    //codes
+                    float number41 = float.Parse(number1);
+                    float number42 = float.Parse(number2);
+                    float sum = number41 + number42;
+                    textBox1.Text = sum.ToString();
+                    break;
+            }
+        }
+
+        private void butx_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "x";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "+";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "/";
+        }
+    }
+}
